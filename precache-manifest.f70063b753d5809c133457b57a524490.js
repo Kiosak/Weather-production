6178d2c5a6bbbf32c3bca189b65b80d731f36d7c
Vadym <40339893+Kiosak@users.noreply.github.com>
2019-06-13 20:40:09 +0300

    Updates

---
+++ precache-manifest.f70063b753d5809c133457b57a524490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b600988afcaf2f2a0155795424b257f",
+    "url": "/Kiosak/Weather-production/index.html"
+  },
+  {
+    "revision": "90b40e43c8d813ea7e4c",
+    "url": "/Kiosak/Weather-production/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "eec135d3ce1bfb6ad294",
+    "url": "/Kiosak/Weather-production/static/css/main.a35427d7.chunk.css"
+  },
+  {
+    "revision": "90b40e43c8d813ea7e4c",
+    "url": "/Kiosak/Weather-production/static/js/2.1d2967ae.chunk.js"
+  },
+  {
+    "revision": "eec135d3ce1bfb6ad294",
+    "url": "/Kiosak/Weather-production/static/js/main.165130a4.chunk.js"
+  },
+  {
+    "revision": "4f6f0fb142c1e475123e",
+    "url": "/Kiosak/Weather-production/static/js/runtime~main.2a621e43.js"
+  },
+  {
+    "revision": "facc40a86a3da5049ab40c03579ff761",
+    "url": "/Kiosak/Weather-production/static/media/FOREST.facc40a8.svg"
+  }
+]);
