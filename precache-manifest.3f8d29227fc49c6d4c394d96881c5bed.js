969c0d05f1679d1a93853af3e3445bec81bb66fb
Vadym <40339893+Kiosak@users.noreply.github.com>
2019-06-13 17:36:04 +0300

    production

---
+++ precache-manifest.3f8d29227fc49c6d4c394d96881c5bed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5be36f90d567e7a5108120e53f6e55c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b40e43c8d813ea7e4c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "37b17fdea6c518c5274a",
+    "url": "/static/css/main.b4ab171d.chunk.css"
+  },
+  {
+    "revision": "90b40e43c8d813ea7e4c",
+    "url": "/static/js/2.1d2967ae.chunk.js"
+  },
+  {
+    "revision": "37b17fdea6c518c5274a",
+    "url": "/static/js/main.192b4612.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "facc40a86a3da5049ab40c03579ff761",
+    "url": "/static/media/FOREST.facc40a8.svg"
+  }
+]);
